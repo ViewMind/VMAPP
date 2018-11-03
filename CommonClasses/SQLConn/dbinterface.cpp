6e73ea37d7dcf576b2d68509caa1c6c5a7842f1a
Ariel Arelovich <aarelovich@gmail.com>
2018-11-03 17:43:49 -0300

    Correcciones al servidor

--- CommonClasses/SQLConn/dbinterface.cpp
+++ CommonClasses/SQLConn/dbinterface.cpp
@@ -82,6 +82,8 @@ bool DBInterface::updateDB(const QString &table, const QStringList &columns, con
 
 bool DBInterface::deleteRowFromDB(const QString &table, const QString &condition){
     QString query = "DELETE FROM " + table + " WHERE " + condition;
+    LogInterface log;
+    log.appendStandard("DELETE QUERY IS: " + query);
     QSqlQuery q;
     if (!q.exec(query)){
         error = "DELETE Error on query: " + query + ". ERROR: " + q.lastError().text();
