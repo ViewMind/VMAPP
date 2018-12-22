9f7fcacbfacfb85b4640a71f1610a55be00bebc4
Ariel Arelovich <aarelovich@gmail.com>
2018-12-22 07:20:52 -0300

    Ultimos cambios, parche funciono para las bases de datos de desarrollos

--- Scripts/server_scripts/dbbkp.sh
+++ Scripts/server_scripts/dbbkp.sh
@@ -16,41 +16,52 @@ elif [[ $1 == "dev" ]]; then
    DNS="18.191.142.5"
    USER="ec2-user"
    DEST="./bkpDev"
-   DB_NAME="viewmind_develop"
+   declare -a DB_NAMES=("viewmind_develop" "viewmind_id_dev" "viewmind_patdata_dev")
+   declare -a HOST_NAMES=("viewminddb" "viewmind-id" "viewmind-patdata")   
+   declare -a FILE_NAMES=("res" "id" "patdata")
    PREFIX="dev"
 else
    echo "Only options are prod or dev"
 fi
 
-# Key
-KEY="/home/ariela/Dropbox/ViewMindData/awspem/aws_ec2_first_time.pem"
-LOCALPASS="vmdb"
-LOCALUSER="vmdb"
+clear
 
 # MYSQL PARAMETERS
-DB_HOST="viewminddb.cdqlb2rkfdvi.us-east-2.rds.amazonaws.com"
+DB_BASE_HOST=".cdqlb2rkfdvi.us-east-2.rds.amazonaws.com"
 DB_PORT="3306"
 DB_USER="root"
 DB_PASS="vimiroot"
+KEY="/home/ariela/Dropbox/ViewMindData/awspem/aws_ec2_first_time.pem"
 
-# The backup
+# The base backup name
 DB_BKP=$PREFIX"_$(date +"%Y_%m_%d_%H_%M").sql"
+LOCALPASS="vmdb"
+LOCALUSER="vmdb"
 
-CREATEBKP="mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USER -p\"$DB_PASS\" --databases $DB_NAME > $DB_BKP"
-DELETEBKP="rm $DB_BKP"
-
-# Creating the BKP
-echo ">> CREATING THE BKP"
-ssh -i $KEY $USER@$DNS "$CREATEBKP"
-
-# Copying it here
-echo ">> COPYING THE BKP"
-scp -i $KEY $USER@$DNS:$DB_BKP $DEST
+total=${#DB_NAMES[@]}
+for (( i=0; i<$total; i++ )); do
    
-# Removing the BKP from the server
-echo ">> DELETING THE ORIGINAL BKP"
-ssh -i $KEY $USER@$DNS "$DELETEBKP"
+   DB_NAME=${DB_NAMES[$i]}   
+   FNAME=${FILE_NAMES[$i]}   
+   FNAME=$FNAME"_"$DB_BKP
+   DB_HOST=${HOST_NAMES[$i]}   
+   DB_HOST=$DB_HOST$DB_BASE_HOST
+   
+   CREATEBKP="mysqldump -h $DB_HOST -P $DB_PORT -u $DB_USER -p\"$DB_PASS\" --databases $DB_NAME > $FNAME"
+   DELETEBKP="rm $FNAME"
+   
+   echo ">> CREATING THE BKP For $DB_NAME named: $FNAME"
+   ssh -i $KEY $USER@$DNS "$CREATEBKP"
+   # Copying it here
+   echo ">> COPYING THE BKP: $FNAME to $DEST"
+   scp -i $KEY $USER@$DNS:$FNAME $DEST
+   echo ">> DELETING THE ORIGINAL BKP"
+   ssh -i $KEY $USER@$DNS "$DELETEBKP"
+   echo ">>>>>>>>>>>>>>>>>>>>>>>>>>> DONE!"
+   
+   # Restoring to the local database.
+   echo ">> RESTORING TO BKP: $FNAME TO LOCAL DB: $DB_NAME"
+   mysql -u $LOCALUSER -p"$LOCALPASS" $DB_NAME < $DEST/$FNAME  
+   
+done
 
-# Restoring to the local database.
-echo ">> RESTORING TO BKP TO LOCAL DB"
-mysql -u $LOCALUSER -p"$LOCALPASS" $DB_NAME < $DEST/$DB_BKP
