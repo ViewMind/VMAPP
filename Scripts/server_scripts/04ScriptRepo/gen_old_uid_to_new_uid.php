138f813e7384180f5b45510ecc6f3ab1772a204e
Ariel Arelovich <aarelovich@gmail.com>
2019-01-27 08:19:17 -0300

    scripting

--- Scripts/server_scripts/04ScriptRepo/gen_old_uid_to_new_uid.php
+++ Scripts/server_scripts/04ScriptRepo/gen_old_uid_to_new_uid.php
@@ -70,7 +70,7 @@
            $query = "INSERT INTO tPaientIDs (uid) VALUES ('" . $parts[1] . "')";
            //echo "$query\n";
            $res = mysqli_query($con_id,$query);
-           if (!res){
+           if (!$res){
               echo "ErrorDB. Query: " . $query . ". El error SQL es: " . mysqli_error($con_id);
               mysqli_close($con_data); mysqli_close($con_id); mysqli_close($con_patdata);     
               return;   
