fead28748279f1b912f6afef4eef82db8d445797
Ariel Arelovich <aarelovich@gmail.com>
2019-02-24 10:47:27 -0300

    Se corrige el parche 5

--- Scripts/server_scripts/00patches/mod05.sh
+++ Scripts/server_scripts/00patches/mod05.sh
@@ -34,74 +34,8 @@ PORT=3306
 export MYSQL_PWD=$PASSWD
 
 echo ">> APPLYING PATCH 05"
-echo ">> Constructing doctor institution map logic"
-queryres=($(mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "SELECT DISTINCT 
-                                                                                    a.uid AS duid, 
-                                                                                    a.keyid, 
-                                                                                    a.medicalinstitution AS medid
-                                                                              FROM tInstitution i, tDoctors a
-                                                                              INNER JOIN (
-                                                                                 SELECT uid, MAX(keyid) maxkeyid
-                                                                                 FROM tDoctors
-                                                                                 GROUP BY uid
-                                                                              ) b ON a.uid = b.uid AND a.keyid = b.maxkeyid
-                                                                             WHERE i.uid = a.medicalinstitution"))
+mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "ALTER TABLE tPlacedProducts ADD COLUMN activation_date DATE DEFAULT '0000-00-00'"
+mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "ALTER TABLE tPlacedProducts ADD COLUMN production_date DATE DEFAULT '0000-00-00'"
 
-# The firs three elements are the column names.
-unset 'queryres[0]'
-unset 'queryres[1]'
-unset 'queryres[2]'
-
-declare -A drmedidmap; 
-counter=0
-drid=""
-for i in "${queryres[@]}"
-do   
-#   echo $i
-   if (( $counter == 0 )); then
-      #echo "DRID: $i"
-      drid=$i
-      counter=$( expr $counter + 1 )
-   elif (( $counter == 2 )); then
-      #echo "MEDID: $i"
-      drmedidmap[$drid]=$i
-      counter=0
-   else
-     counter=$( expr $counter + 1 )
-   fi
-done        
-
-for K in "${!drmedidmap[@]}"; 
-do 
-  value=${drmedidmap[$K]}
-  newvalue=$K"_"$value
-  echo "$K -> $value -> $newvalue"
-done
-
-echo ">> Changing all doctor's id from the tDoctors table"
-for K in "${!drmedidmap[@]}"; 
-do 
-  value=${drmedidmap[$K]}
-  newvalue=$K"_"$value
-  mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "UPDATE tDoctors SET uid = '$newvalue' WHERE uid = '$K'"
-done
-
-echo ">> Changing all doctor's id from the tEyeResults table"
-for K in "${!drmedidmap[@]}"; 
-do 
-  value=${drmedidmap[$K]}
-  newvalue=$K"_"$value
-  mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "UPDATE tEyeResults SET doctorid = '$newvalue' WHERE doctorid = '$K'"
-done
-echo ">> Changing all doctor's id from the tPatientData table"
-for K in "${!drmedidmap[@]}"; 
-do 
-  value=${drmedidmap[$K]}
-  newvalue=$K"_"$value
-  mysql -h $DB_PATDATA_HOST -u $USER -P $PORT --database=$DB_PATDATA_NAME -e "UPDATE tPatientData SET doctorid = '$newvalue' WHERE doctorid = '$K'"
-done
-
-# mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "ALTER TABLE tPlacedProducts ADD COLUMN enabled TINYINT;"
-# mysql -h $DB_RES_HOST -u $USER -P $PORT --database=$DB_RES_NAME -e "UPDATE tPlacedProducts SET enabled = '1';"
 
 echo "Finished"
