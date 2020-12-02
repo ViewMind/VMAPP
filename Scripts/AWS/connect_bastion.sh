63610351d876e5d2d5af71a548a9dc75afe075df
Ariel Arelovich <aarelovich@gmail.com>
2020-12-02 01:37:49 -0300

    Added basic connection scripts for aws

---
+++ Scripts/AWS/connect_bastion.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#ssh -i aws_ec2_first_time.pem ec2-user@3.139.215.159
+ssh  ec2-user@3.139.215.159
+
+# Bastion Public Key. 
+# ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC+35n9MF8mVyddGj1XUe++nT+bvUU6ImLfJ5wUp88ZBhudVi5buuaG2xILLcbtqHpmj2NspD012IjtMkyvRYm9/ImTJS75+XLCeNUFoc08adlv0XvAFlGUckEKFoRri72EiMpxCkpMycl1f2nQE18f4mbhPtwxgiduzMkStXw6/Du/S25REQs/GQ7P3QKQhwhKBiQ0X7EEiepLN6M7C/cNnrOmNxdlLMBWpBQi9xL6BnHr1VKcr1yibpNcALegjExwNFRIWkGE4Le10y1fcVOL8LU6cXr8saKQVbutXdL+NIAgSXnQX/gfGPOQB/R/ki/bh9WS6g6y5065I6KDr9BL ec2-user@ip-10-100-101-126.us-east-2.compute.internal
