58d9849ea2cdd22e2c376d7fe3720990100e0ebc
Ariel Arelovich <aarelovich@gmail.com>
2021-03-02 06:18:47 -0300

    Added new adddress to internal AWS Connect Script

--- Scripts/AWS/connect_internal.sh
+++ Scripts/AWS/connect_internal.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 
-if [[ $1 == "proc" ]]; then
+if [[ $1 == "rawdataserver1" ]]; then
    ssh ec2-user@10.100.6.150
 elif [[ $1 == "web" ]]; then
    ssh ec2-user@10.100.14.154
+elif [[ $1 == "proc1" ]]; then   
+   ssh ec2-user@10.100.15.115
 else
    echo "Do not know $1"
    exit;
