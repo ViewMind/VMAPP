882f5fe773110321edb29eabbdce360f3aec27bb
Ariel Arelovich <aarelovich@gmail.com>
2021-03-13 05:43:19 -0300

    Un poco de limpieza de php

--- Scripts/inst_data_available/sh_secondary_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-echo "Some Text 1"
-echo "Now an error"
-
-cp non_existant_file 2
--- Scripts/inst_data_available/sh_tests.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-LOG=$(date '+%Y_%m_%d_%H_%M_%S.log')
-php php_log_tests.php > $LOG 2>&1
