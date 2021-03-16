2deec31b2bb8b7b46f82bd901b32b4dde5dadd63
Ariel Arelovich <aarelovich@gmail.com>
2021-03-16 17:17:09 -0300

    Se corrige el ip interno dle nuevo servidor web

--- Scripts/AWS/connect_internal.sh
+++ Scripts/AWS/connect_internal.sh
@@ -3,7 +3,7 @@
 if [[ $1 == "rawdataserver1" ]]; then
    ssh ec2-user@10.100.6.150
 elif [[ $1 == "web" ]]; then
-   ssh ec2-user@10.100.14.154
+   ssh ec2-user@10.100.7.77
 elif [[ $1 == "proc1" ]]; then   
    ssh ec2-user@10.100.15.115
 else
