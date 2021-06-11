e96cd601ea75fe14cda60e0c16f717edf61899ba
Ariel Arelovich <aarelovich@gmail.com>
2021-06-11 04:56:55 -0300

    Created different directories for AWS EU and US. New scripts and removed a file that should not be in the repos

---
+++ Scripts/AWS/EU/authorize_my_ip.sh
@@ -0,0 +1,16 @@
+# Retrieve current IP address
+
+export AWS_PROFILE=viewmind_eu
+
+SG='sg-062c35c429ee27ece'
+DESC='ArielArelovich'
+IP=`curl -s http://whatismyip.akamai.com/`
+
+OLD_IP=$(aws ec2 describe-security-groups --group-id $SG --query "SecurityGroups[0].IpPermissions[0].IpRanges[?Description=='${DESC}'].CidrIp" --output text)
+#echo "$OLD_IP"
+aws ec2 revoke-security-group-ingress --group-id $SG --ip-permissions IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges="[{CidrIp=$OLD_IP,Description='${DESC}'}]"  --output text
+aws ec2 authorize-security-group-ingress --group-id $SG --ip-permissions IpProtocol=tcp,FromPort=22,ToPort=22,IpRanges="[{CidrIp=$IP/32,Description='${DESC}'}]"  --output text
+# aws ec2 revoke-security-group-ingress --group-id $SG --ip-permissions IpProtocol=tcp,FromPort=3306,ToPort=3306,IpRanges="[{CidrIp=$OLD_IP,Description='${DESC}'}]"  --output text
+# aws ec2 authorize-security-group-ingress --group-id $SG --ip-permissions IpProtocol=tcp,FromPort=3306,ToPort=3306,IpRanges="[{CidrIp=$IP/32,Description='${DESC}'}]"  --output text
+# aws ec2 revoke-security-group-ingress --group-id $SG --ip-permissions IpProtocol=tcp,FromPort=3307,ToPort=3307,IpRanges="[{CidrIp=$OLD_IP,Description='${DESC}'}]"  --output text
+# aws ec2 authorize-security-group-ingress --group-id $SG --ip-permissions IpProtocol=tcp,FromPort=3307,ToPort=3307,IpRanges="[{CidrIp=$IP/32,Description='${DESC}'}]"  --output text
---
+++ Scripts/AWS/EU/connect_bastion.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+ssh  ec2-user@3.122.211.57
